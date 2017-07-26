387c45f11c06923d2294dd1060fdd89b5c242019
aganglada <aganglada@gmail.com>
2017-07-26 15:19:48 +0100

    replace React.createClass with stateless component

--- src/svgTemplate.js
+++ src/svgTemplate.js
@@ -1,14 +1,12 @@
 var React = require('react');
 
-module.exports = React.createClass({
-    render: function() {
-        var baseProps = {
-            className: '{% className %}',
-            dangerouslySetInnerHTML: {
-                __html: '{% svgElement %}'
-            }
-        };
-        var props = Object.assign({}, baseProps, this.props);
-        return React.createElement('i', props);
-    }
-});
+module.exports = function(props) {
+    const baseProps = {
+        className: '{% className %}',
+        dangerouslySetInnerHTML: {
+            __html: '{% svgElement %}'
+        }
+    };
+
+    return React.createElement('i', Object.assign({}, baseProps, props));
+};
